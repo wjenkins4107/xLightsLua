93408bd5e962259dd30466090ba389dad5ff3abd
wjenkins4107 <56661476+wjenkins4107@users.noreply.github.com>
2023-08-13 08:51:15 -0400

    Add files via upload

---
+++ CleanupFileLocations.lua
@@ -0,0 +1,17 @@
+seqs = PromptSequences()
+for i,seq in ipairs(seqs) do 
+    properties = {}
+    properties['seq'] = seq
+
+    result = RunCommand('openSequence', properties)
+    Log(result['seq']) 
+    result = RunCommand('cleanupFileLocations', {})
+    Log(result['msg'])
+    result = RunCommand('saveSequence', {})
+    Log(result['msg'])
+    result = RunCommand('closeSequence', {})
+    Log(result['msg'])
+end
+result = RunCommand('saveLayout', {})
+Log(result['msg']) 
+
---
+++ uploadControllers.lua
@@ -0,0 +1,15 @@
+result = RunCommand('getControllerIPs', {})
+Log('result = ' .. result['res'])
+controllers = result['controllers']
+arraylength = #controllers
+for i=0,arraylength do
+    IP = controllers[i]
+	Log('IP =' .. IP)
+	parm = {}
+	parm['ip'] = IP
+    result = RunCommand('uploadController', parm)	
+	Log('result = ' .. result['msg'])
+end
+
+
+
